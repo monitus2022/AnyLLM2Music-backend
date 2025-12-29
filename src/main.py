38f4804e0b293f8116dea9908dfd80ae18218953
monitus2022 <monitus202201@proton.me>
2025-12-29 21:32:27 +0800

    updated CORS to allow cloudflare pages

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ app = FastAPI(title="AnyLLM2Music", version="0.1.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://anyllm2music-frontend.pages.dev/", "https://music.monitus.org/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
